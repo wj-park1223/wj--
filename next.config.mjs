0783ca6136e84123ee63594c29b4ebae341c7687
wj-park1223 <pwj021223@naver.com>
2024-10-01 17:17:59 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,3 @@ const nextConfig = {};
 
 export default nextConfig;
 
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/public/:path*',
-          destination: '/:path*',
-        },
-      ];
-    },
-  };
--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,14 @@ export default function home() {
           <Link href="/">게임소식</Link>
           <Link href="/">게임할인</Link>
       </div>
-      <div className="bar2">
-      <img src="/sky box.png"></img>        
+      <div className="bar2">       
           <Link href="/">신작게임</Link>
           <Link href="/">인기게임</Link>
       </div>
+      <div class="search">
+        <input id="search-input" placeholder="검색어를 입력하세요" />
+      </div>
+    
     
     </div>
   );
